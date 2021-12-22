736434d6692ef15e76089f7a0f31fdfe3aa52d22
villacris <cristymaevilla@gmail.com>
2021-12-22 16:49:12 +0800

    create two functions for multiple lcs result

--- exam.js
+++ exam.js
@@ -6,22 +6,57 @@ let string1 = "ajblqcpdz";
 let string2 = "aefcnbtdi";
 // expected outcome:abd,acd
 
-// simple looping:
-// problem:only returns one lcs
+// through simple looping:
 let longestCommmonSubsequence = (string1, string2)=>{
-	  let result=""; 
-	  let startingIndex=0;
-	  for (let i = 0; i < string1.length; i += 1) {
-	       match=()=>{ 
-	            for (let b = startingIndex; b < string2.length; b += 1) {
-	                if(string1[i]=== string2[b]){ 
-	                    return startingIndex= b+1, result += string1[i]
-	                    
-	                  }
-	            }
-	       }
-	       match();
-	  }
-	  return result;
+let arr =[];
+cs1=()=>{
+let startingIndex=0;
+  let result="";  
+  for (let i = 0; i < string1.length; i += 1) {
+    console.log(string1[i]);
+    console.log(startingIndex)
+       findMatch=()=>{ 
+            for (let b = startingIndex; b < string2.length; b += 1) {
+                if(string1[i]=== string2[b]){ 
+                    startingIndex= b+1; 
+                    result += string1[i]; 
+                    break;
+                    
+                  }
+            }
+       }
+       findMatch();
+  }
+  return result;}
+
+
+cs2=()=>{
+   let result=""; 
+   let startingIndex=0;
+   for (let a = 0; a < string2.length; a += 1) {
+        findMatch=()=>{ 
+            // console.log(startingIndex);
+             for (let b = startingIndex; b < string1.length; b += 1) {
+                 if(string2[a]=== string1[b]){ 
+                     startingIndex= b+1;
+                      result += string2[a];
+                      break;
+                     
+                   }
+             }
+        }
+        findMatch();
+   }
+   return result;
 };
-console.log(longestCommmonSubsequence(string1, string2));
+
+ let a1 =[cs1(), cs2()];
+ if(a1[0]=== a1[1]){return a1[0]}
+ if(a1[0].length > a1[1].length){return a1[0]}
+ if(a1[0].length < a1[1].length){return a1[1]}
+ if(a1[0].length = a1[1].length && a1[0]!== a1[1]){return a1}
+ else{return "no common subsequence found"}
+ 
+};
+longestCommmonSubsequence(string1,string2);
+console.log ("answer:"+ longestCommmonSubsequence(string1,string2))
