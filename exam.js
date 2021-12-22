9f3b2fad30bd032e73eb8da8ebbdbba5239f2f32
villacris <cristymaevilla@gmail.com>
2021-12-22 16:12:30 +0800

    Initial commit

---
+++ exam.js
@@ -0,0 +1,6 @@
+// LONGEST COMMON SUBSEQUENCE OF TWO STRINGS:
+// note: NO string functions , NO recursions
+let string1 = "ajblqcpdz";
+let string2 = "aefcnbtdi";
+
+let longestCommmonSubsequence = (string1, string2)=>{};
