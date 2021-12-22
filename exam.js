ec2bf40397e4391f35b60a276c73f6d4c8b02e46
villacris <cristymaevilla@gmail.com>
2021-12-22 16:24:13 +0800

    Use for loop to find lcs

--- exam.js
+++ exam.js
@@ -1,6 +1,27 @@
 // LONGEST COMMON SUBSEQUENCE OF TWO STRINGS:
 // note: NO string functions , NO recursions
+/*options: looping (simple, easy to understand)
+ or dynamic programming (good for finding the length of lcs)*/
 let string1 = "ajblqcpdz";
 let string2 = "aefcnbtdi";
+// expected outcome:abd,acd
 
-let longestCommmonSubsequence = (string1, string2)=>{};
+// simple looping:
+// problem:only returns one lcs
+let longestCommmonSubsequence = (string1, string2)=>{
+	  let result=""; 
+	  let startingIndex=0;
+	  for (let i = 0; i < string1.length; i += 1) {
+	       match=()=>{ 
+	            for (let b = startingIndex; b < string2.length; b += 1) {
+	                if(string1[i]=== string2[b]){ 
+	                    return startingIndex= b+1, result += string1[i]
+	                    
+	                  }
+	            }
+	       }
+	       match();
+	  }
+	  return result;
+};
+console.log(longestCommmonSubsequence(string1, string2));
